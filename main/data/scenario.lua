c593a25006ec878253401731ed41b6cb91c57195
Christopher Kaster <me@atomicptr.de>
2025-07-20 20:11:36 +0200

    add scenario system to test well scenarios

---
+++ main/data/scenario.lua
@@ -0,0 +1,60 @@
+local Grid = require "modules.grid"
+local Ruleset = require "main.data.rulesets"
+local cards = require "main.data.cards"
+
+local Pos = Grid.Pos
+
+---@class Scenario
+---@field start_turn "player"|"enemy"
+---@field player_hand integer[]
+---@field enemy_hand integer[]
+---@field rules Ruleset[]
+---@field field GameField
+
+---@param steps { pos: BoardPos, element: Element|nil, card: integer, owner: "player"|"enemy" }
+---@return GameField
+local function make_grid(steps)
+    ---@type GameField
+    local grid = Grid.create(function(row, col)
+        ---@type CardSlot
+        return {
+            card = nil,
+            row = row,
+            col = col,
+            element = nil,
+        }
+    end)
+
+    for _, step in ipairs(steps) do
+        local pos = step.pos
+
+        grid[pos.row][pos.col].element = step.element
+        grid[pos.row][pos.col].card = cards[step.card]
+        grid[pos.row][pos.col].owner = step.owner
+    end
+
+    return grid
+end
+
+---@type table<string, Scenario>
+local scenarios = {
+    TestSameCombo = {
+        start_turn = "player",
+        player_hand = { 40, 40, 40 },
+        enemy_hand = { 1, 1 },
+        rules = {
+            Ruleset.Open,
+            Ruleset.Same,
+            Ruleset.Combo,
+        },
+        field = make_grid {
+            { pos = { row = Pos.Center, col = Pos.West }, card = 40, owner = "player" },
+            { pos = { row = Pos.Center, col = Pos.East }, card = 40, owner = "enemy" },
+            { pos = { row = Pos.South, col = Pos.Center }, card = 54, owner = "enemy" },
+            { pos = { row = Pos.South, col = Pos.West }, card = 71, owner = "enemy" },
+            { pos = { row = Pos.South, col = Pos.East }, card = 13, owner = "enemy" },
+        },
+    },
+}
+
+return scenarios
