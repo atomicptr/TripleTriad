abd87e2cf21d4e1ce9bcc959d246a5ebcc84acbf
Christopher Kaster <me@atomicptr.de>
2025-07-26 21:35:56 +0200

    add drag & drop mouse controls

--- main/screen.lua
+++ main/screen.lua
@@ -12,4 +12,16 @@ function M.world_to_screen(pos)
     return vmath.vector3(screen_x, screen_y, 0)
 end
 
+---@param pos vector3
+---@return vector3
+function M.screen_to_world(pos)
+    local screen_width, screen_height = window.get_size()
+    local zoom = window.get_display_scale()
+
+    local world_x = (pos.x - screen_width * 0.5) / zoom
+    local world_y = (pos.y - screen_height * 0.5) / zoom
+
+    return vmath.vector3(world_x, world_y, 0)
+end
+
 return M
